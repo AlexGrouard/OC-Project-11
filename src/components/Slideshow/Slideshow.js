f880a85e1325c2f7ade03a1b3ba85868684e4d9e
AlexFreyjr <alexfreyjr@gmail.com>
2023-04-11 10:36:27 +0200

    add button for slideshow

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,11 +1,13 @@
 import styles from "./Slideshow.module.scss"
+import arrowLeft from "assets/UI/arrow-left.png"
+import arrowRight from "assets/UI/arrow-right.png"
 import { useState } from "react"
 
 function Slideshow({ images }) {
 	let [currentSlide, setCurrentSlide] = useState(0) //to do more research to understand how the hook works
 
 	const handleClick = () => {
-		console.log(currentSlide)
+		//console.log(currentSlide)
 		//move up the list of images and at the max set back to 0
 		if (currentSlide < images.length) {
 			setCurrentSlide(currentSlide + 1)
@@ -14,23 +16,39 @@ function Slideshow({ images }) {
 				setCurrentSlide((currentSlide = 0))
 			}
 		}
-		/*to use with swipe gesture
-        if (currentSlide > images.length) {
-            setCurrentSlide(currentSlide - 1)
-            if (currentSlide === images.length) {currentSlide = images.length}
-        } */
+		if (currentSlide > images.length) {
+			setCurrentSlide(currentSlide - 1)
+			if (currentSlide === images.length) {
+				currentSlide = images.length
+			}
+		}
 	}
 
 	return (
-		<button
-			className={styles.slideshow}
-			onClick={handleClick}>
-			<img
-				className={styles.image}
-				src={images[currentSlide]}
-				alt={`Slide ${currentSlide}`}
-			/>
-		</button>
+		<div className={styles.main}>
+			<button className={styles.slideshow} onClick={handleClick}>
+				<img
+					className={styles.image}
+					src={images[currentSlide]}
+					alt={`Slide ${currentSlide}`}
+				/>
+			</button>
+			{/* need to implement a rollback to the start*/}
+			<div className={styles.control}>
+				<button
+					className={styles.previousBtn}
+					onClick={() => setCurrentSlide(currentSlide - 1)}
+				>
+					<img src={arrowLeft} alt='previous' />
+				</button>
+				<button
+					className={styles.nextBtn}
+					onClick={() => setCurrentSlide(currentSlide + 1)}
+				>
+					<img src={arrowRight} alt='next' />
+				</button>
+			</div>
+		</div>
 	)
 }
 
